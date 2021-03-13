6c7fcb4841df83a32df9fb9a4200c123840cf6be
lclllllu <79777184+lclllllu@users.noreply.github.com>
2021-03-13 00:26:42 -0500

    03/11/21

--- 56.java
+++ 56.java
@@ -10,17 +10,17 @@ class Solution {
         List<int[]> result = new ArrayList<>();
         
         int[] lastInterval = intervals[0];
-		result.add(lastInterval);
-        
-		for (int[] interval : intervals) {
+	result.add(lastInterval);
+	    
+	for (int[] interval : intervals) {
             if (interval[0] <= lastInterval[1]) {
-				lastInterval[1] = Math.max(lastInterval[1], interval[1]);
+		lastInterval[1] = Math.max(lastInterval[1], interval[1]);
             } else {
-				lastInterval = interval;
-				result.add(lastInterval);
-			}
-		}
-
-		return result.toArray(new int[result.size()][]);
+		lastInterval = interval;
+		result.add(lastInterval);
+	    }
+	}
+	    
+	return result.toArray(new int[result.size()][]);
     }
 }
